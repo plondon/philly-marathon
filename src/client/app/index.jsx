462331002ccdb30ab5e3d873cbe6ae5c4e70d3ab
plondon <plondon@gm.slc.edu>
2016-11-06 10:47:46 -0500

    Move google maps into react component

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {GoogleMapLoader, GoogleMap, Polyline} from "react-google-maps";
+
+export default function Map (props) {
+  return (
+    <section style={{height: "100%"}}>
+      <GoogleMapLoader
+        containerElement={
+          <div
+            {...props.containerElementProps}
+            style={{
+              height: "100%",
+            }}
+          />
+        }
+        googleMapElement={
+          <GoogleMap
+            ref={(map) => console.log(map)}
+            defaultZoom={13}
+            defaultCenter={{ lat: props.run.start_latlng[0], lng: props.run.start_latlng[1] }}
+            onClick={props.onMapClick}
+          >
+            <Polyline
+              path={decodePath(props.run.map.summary_polyline)}
+              levels={decodeLevels('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')}
+            />
+          </GoogleMap>
+        }
+      />
+    </section>
+  );
+}
+
+class Run extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.onRunChange(this.props.run)
+  }
+
+  render () {
+    var name = this.props.run.name;
+    var date = this.props.run.date;
+    var start = this.props.run.start_latlng;
+    var polyline = this.props.run.map.summary_polyline;
+
+    return (
+      <a onClick={this.handleClick}>{name}</a>
+    )
+  }
+}
 
 class RunList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render () {
     return (
       <ol>
         {this.props.runs.map((run) => {
           return <li key={run.id}>
-                    <a data-start={run.start_latlng}
-                       data-polyline={run.map.summary_polyline}>
-                      {run.name}
-                    </a>
+                    <Run run={run} onRunChange={this.props.onRunChange}/>
                   </li>
         })}
       </ol>
@@ -18,48 +72,46 @@ class RunList extends React.Component {
   }
 }
 
-var myLatlng = new google.maps.LatLng(40, -74);
-var map = new google.maps.Map(document.getElementById('map'), {center: myLatlng});
-
 $.get('/runs', function (runs) {
-  render(<RunList runs={runs}/>, document.getElementById('runs'))
-});
+  class RunTracker extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        run: runs[0]
+      }
 
-$('#runs').on('click', 'a', function (e) {
-  e.preventDefault();
-  $('#map').addClass('active');
-
-  var polyline = $(e.currentTarget).data('polyline');
-  var startLatLng = $(e.currentTarget).data('start').split(',');
-
-  var myLatlng = new google.maps.LatLng(startLatLng[0], startLatLng[1]);
-
-  var myOptions = {
-    zoom: 13,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById('map'), myOptions);
-
-  var decodedPath = google.maps.geometry.encoding.decodePath(polyline);
-  var decodedLevels = decodeLevels('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB');
-
-  new google.maps.Polyline({
-    path: decodedPath,
-    levels: decodedLevels,
-    strokeColor: '#4285F4',
-    strokeOpacity: 0.8,
-    strokeWeight: 3,
-    map: map
-  });
-
-  function decodeLevels (encodedLevelsString) {
-    var decodedLevels = [];
-
-    for (var i = 0; i < encodedLevelsString.length; ++i) {
-      var level = encodedLevelsString.charCodeAt(i) - 63;
-      decodedLevels.push(level);
+      this.handleRunChange = this.handleRunChange.bind(this);
     }
-    return decodedLevels;
+
+    handleRunChange(run) {
+      this.setState({
+        run: run
+      });
+    }
+
+    render () {
+      return (
+        <div>
+          <RunList runs={runs} onRunChange={this.handleRunChange}/>
+          <Map run={this.state.run}/>
+        </div>
+      )
+    }
   }
+
+  render(<RunTracker/>, document.getElementById('main'))
 });
+
+function decodePath (pathString) {
+  return google.maps.geometry.encoding.decodePath(pathString);
+}
+
+function decodeLevels (encodedLevelsString) {
+  var decodedLevels = [];
+
+  for (var i = 0; i < encodedLevelsString.length; ++i) {
+    var level = encodedLevelsString.charCodeAt(i) - 63;
+    decodedLevels.push(level);
+  }
+  return decodedLevels;
+}
